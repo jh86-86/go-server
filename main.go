bcc659318a6b2b149040928d4ea8cd914075e769
JohnJames Hodgins <johnjameshodgins@JohnJamess-MacBook-Pro.local>
2021-07-07 08:35:42 +0100

    CRUD funcs set up and structs

--- main.go
+++ main.go
@@ -1,10 +1,73 @@
 package main
 
-import "net/http" //contains all things we need
+import(
+	"encoding/json"
+	"log"
+	"net/http"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+
+)
+
+//Book strut is basically model , kind of like class, OOP
+
+type Book struct {
+	ID string `json: "id"`
+	Isbn string `json: "isbn"`
+	title string `json: "title"`
+	Author *Author `json: "author"`
+}
+
+//Author struct
+
+type Author struct {
+	Firstname string `json: "json: firstname"`
+	Lastname string `json: "json: lastname"`
+
+}
+
+
+//get all books
+
+func getBooks(w http.ResponseWriter, r *http.Request) { //response and request types
+	
+}
+
+//get single book
+func getBook(w http.ResponseWriter, r *http.Request) { //response and request types
+
+}
+
+
+func createBook(w http.ResponseWriter, r *http.Request) { //response and request types
+
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) { //response and request types
+
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) { //response and request types
+
+}
+
+
 
 func main(){
-	http.HandleFunc("/hello-world", func (w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Hello world")) //casted into a byte array
-	})
-	http.ListenAndServe(":8080", nil) //nil not important for now
+	//Init router
+	r := mux.NewRouter();
+
+	//route handlers / endpoints
+
+	r.HandleFunc("/api/books", getBooks).Methods("GET")  //needs method to know what type of http method is needed
+	r.HandleFunc("/api/book/{id}", getBook).Methods("GET")  //needs method to know what type of http method is needed
+	r.HandleFunc("/api/books", createBook).Methods("Post")  //needs method to know what type of http method is needed
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")  //needs method to know what type of http method is needed
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE") 
+	
+	log.Fatal(http.ListenAndServe(":8000", r)) //r is the router and ListenAndServe is similar to nodes listen,log fatal gets an error if it fails
 }
+
+
+
